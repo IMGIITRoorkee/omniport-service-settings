a576048ba56bf25fb361152610f0631f40edd790
Dhruv Bhanushali <dhruv_b@live.com>
2018-12-16 23:25:22 +0530

    Fix a few bugs in the session map viewset

--- views/session_map.py
+++ views/session_map.py
@@ -1,3 +1,6 @@
+from importlib import import_module
+
+from django.conf import settings
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 
@@ -24,7 +27,11 @@ class SessionMapViewSet(
         :param instance: the SessionMap instance to destroy
         """
 
-        instance.clear_session()
+        session_key = instance.session_key
+        SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
+        session = SessionStore(session_key=session_key)
+        session.delete()
+
         return super().perform_destroy(instance)
 
     def get_queryset(self):
@@ -33,4 +40,5 @@ class SessionMapViewSet(
         :return: the list of all user SessionMap objects
         """
 
-        return self.request.user.sessionmap_set.all()
+        user = self.request.user
+        return user.sessionmap_set.all().order_by('-datetime_modified')
